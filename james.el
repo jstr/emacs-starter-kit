c746b5b5db39293f1ba1274e2142e0fca29fca54
James Stradling <jt.stradling@gmail.com>
2010-01-03 10:00:41 +1300

    Added support for shift-left and shift-right on region.

--- james.el
+++ james.el
@@ -13,6 +13,27 @@
 ;; Custom key binding for ido-imenu
 (global-set-key (kbd "C-.") 'ido-imenu)
 
+;; Tab shift support
+(defun shift-region (distance)
+  (let ((mark (mark)))
+    (save-excursion
+      (indent-rigidly (region-beginning) (region-end) distance)
+      (push-mark mark t t)
+      ;; Tell the command loop not to deactivate the mark
+      ;; for transient mark mode
+      (setq deactivate-mark nil))))
+
+(defun shift-right ()
+  (interactive)
+  (shift-region 1))
+
+(defun shift-left ()
+  (interactive)
+  (shift-region -1))
+
+(global-set-key (kbd "s-]") 'shift-right)
+(global-set-key (kbd "s-[") 'shift-left)
+
 ;; Kill current buffer without prompting
 (global-set-key (kbd "C-c k") 'kill-buffer-now)
 (defun kill-buffer-now ()
